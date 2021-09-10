b753248baa457f3087302d132f514e92767fc3b7
Kevin Pugh <kwpugh@comcast.net>
2021-09-09 20:13:18 -0500

    re-wrote ring of teleport logic

--- src/main/java/com/kwpugh/ring_of_teleport/RingOfTeleport.java
+++ src/main/java/com/kwpugh/ring_of_teleport/RingOfTeleport.java
@@ -13,11 +13,13 @@ public class RingOfTeleport implements ModInitializer
 {
     public static final String MOD_ID = "ring_of_teleport";
     public Item RING_OF_TELEPORT = new ItemRingTeleport(new Item.Settings().group(ItemGroup.MISC));
+    public Item RING_OF_TELEPORT2 = new ItemRingTeleport2(new Item.Settings().group(ItemGroup.MISC));
     public static final ModConfig CONFIG = AutoConfig.register(ModConfig.class, PartitioningSerializer.wrap(JanksonConfigSerializer::new)).getConfig();
 
     @Override
     public void onInitialize()
     {
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "ring_of_teleport"), RING_OF_TELEPORT);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "ring_of_teleport2"), RING_OF_TELEPORT2);
     }
 }
